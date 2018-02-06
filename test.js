59ee8f1789f00944cad2f12dd9531726ce2e44c9
Mateusz Wasieczko <drkillder@tlen.pl>
2018-02-06 12:20:18 +0100

    clean test

--- test.js
+++ test.js
@@ -9,12 +9,6 @@ test('should pass', t => {
         [42, 75],
         [57, 87],
         [59, 81]
-        // [1, 2],
-        // [2, 3],
-        // [3, 4],
-        // [4, 5],
-        // [5, 6],
-        // [6, 7]
     ];
 
     const expected = linearRegression(given);
@@ -22,7 +16,5 @@ test('should pass', t => {
     t.deepEqual(expected, {
         a: 65.1415715245131,
         b: 0.38522498321020804
-        // a: 1,
-        // b: 1
     });
 });
